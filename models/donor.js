c64cae594158b16c05421a646218c5a3be3c3cb4
anushka30aug <anushkashukla3003@gmail.com>
2025-03-11 16:54:33 +0530

    :zap: update schema

--- models/donor.js
+++ models/donor.js
@@ -5,6 +5,7 @@ const Donor = new Schema({
     username: { type: String, required: true },
     profilePic: { type: String },
     contact: { type: Number, length: 10 },
+    email:{type:String , unique:true , require:true},
     rank: { type: Number },
     donationCount: { type: Number }
 })
