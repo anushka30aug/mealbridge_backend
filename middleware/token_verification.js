445e97941b85a871f3aef0b327f7441629f6c47b
Pratyush Namdev <pratyushnamdev140@gmail.com>
2025-03-24 15:05:46 +0530

    :zap: add eslint

--- middleware/token_verification.js
+++ middleware/token_verification.js
@@ -3,24 +3,23 @@ const sendResponse = require("../utils/send_response");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const tokenVerification = (req, res, next) => {
-    try {
-        const authHeader = req.header("Authorization");
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            res.status(401);
-            console.log(res);
-            throw new Error("Invalid or missing authentication token")
-        }
-
-        const token = authHeader.split(" ")[1];
-        const decodedUser = jwt.verify(token, JWT_SECRET);
-        req.user = decodedUser;
-
-        return next();
-    } catch (err) {
-           
-        // return res.status(401).json({ error: true, message: "Authentication failed" });
-        return sendResponse(res,false,err.message);
+  try {
+    const authHeader = req.header("Authorization");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401);
+      console.log(res);
+      throw new Error("Invalid or missing authentication token");
     }
+
+    const token = authHeader.split(" ")[1];
+    const decodedUser = jwt.verify(token, JWT_SECRET);
+    req.user = decodedUser;
+
+    return next();
+  } catch (err) {
+    // return res.status(401).json({ error: true, message: "Authentication failed" });
+    return sendResponse(res, false, err.message);
+  }
 };
 
 module.exports = tokenVerification;
