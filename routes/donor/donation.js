368aa7f535be52753648c444f34a5400da120795
Pratyush Namdev <pratyushnamdev140@gmail.com>
2025-04-04 16:47:44 +0530

    :zap: add meals routes

--- routes/donor/donation.js
+++ routes/donor/donation.js
@@ -4,6 +4,22 @@ const tokenVerification = require("../../middleware/token_verification");
 const donationController = require("../../controllers/donor/donation");
 
 route.post("/post-meal", tokenVerification, donationController.postMeal);
-route.get("/active-meal", tokenVerification, donationController.getActiveMeals);
+route.delete("/cancel-meal", tokenVerification, donationController.cancelMeal);
+
+route.get(
+  "/get-active-meal",
+  tokenVerification,
+  donationController.getActiveMeals
+);
+route.put(
+  "/discard-meal-request",
+  tokenVerification,
+  donationController.discardMealRequest
+);
+route.get(
+  "/get-meal-history",
+  tokenVerification,
+  donationController.getMealHistory
+);
 
 module.exports = route;
