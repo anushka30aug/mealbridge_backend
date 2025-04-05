b99a968034dd8248479625595d62389234528ff8
Pratyush Namdev <pratyushnamdev140@gmail.com>
2025-04-05 15:16:06 +0530

    :zap: add global error handler

--- controllers/universal/user/profile.js
+++ controllers/universal/user/profile.js
@@ -5,28 +5,20 @@ const sendResponse = require("../../../utils/send_response");
 const collector = require("../../../models/collector");
 
 exports.fetchProfile = asyncHandler(async (req, res) => {
-  try {
-    const userType = req.header("User-Type");
-    if (!userType || !["donor", "collector"].includes(userType.toLowerCase())) {
-      throw new ServerError("Invalid or missing User-Type header", 400);
-    }
-
-    const Model = userType.toLowerCase() === "donor" ? Donor : collector;
-
-    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      throw new ServerError("Invalid user ID format", 400);
-    }
-    const user = await Model.findById(req.params.id);
-
-    if (!user) {
-      throw new ServerError("User not found", 404);
-    }
-    sendResponse(res, 200, "User profile fetched successfully", user);
-  } catch (error) {
-    sendResponse(
-      res,
-      error.statusCode || 500,
-      error.message || "Internal Server Error"
-    );
+  const userType = req.header("User-Type");
+  if (!userType || !["donor", "collector"].includes(userType.toLowerCase())) {
+    throw new ServerError("Invalid or missing User-Type header", 400);
   }
+
+  const Model = userType.toLowerCase() === "donor" ? Donor : collector;
+
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    throw new ServerError("Invalid user ID format", 400);
+  }
+  const user = await Model.findById(req.params.id);
+
+  if (!user) {
+    throw new ServerError("User not found", 404);
+  }
+  sendResponse(res, 200, "User profile fetched successfully", user);
 });
